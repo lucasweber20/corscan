2ceb6d2d102847cd840b6fdc55e515da08f885f3
lucasweber20 <lucasweber449@gmail.com>
2026-07-16 10:31:16 -0300

    Feat: arguments flags

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-u", "--url", help="Set url. Example: -u www.example.com", type=str)
+parser.add_argument("-l", "--list", help="Add urls file. Example -u urls.txt", type=str)
+parser.add_argument("-o", "--output", help="Save urls in file. Example: -o urls.txt", type=str)
+
+args = parser.parse_args()
 
 def main():
     pass
